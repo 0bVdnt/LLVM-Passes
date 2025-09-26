5b8226a92bfd6dc411b22cd3f6c6a241a6b763a9
0bVdnt <vedantneve13@gmail.com>
2025-09-26 17:04:48 +0530

    Report Generation

---
+++ StringEncryptAndReportGen/test_program.c
@@ -0,0 +1,16 @@
+// test_program.c
+#include <stdio.h>
+
+void sayHello() { printf("Hello from test program #6969 !Chakravyuha."); }
+
+int sum(int abra, int cadabra) {
+  int abracadabra = abra + cadabra;
+  return abracadabra;
+}
+
+int main() {
+  printf("ABCxyz123\n");
+  sayHello();
+  printf("SUM = Abracadabra = %d", sum(897897, 99875));
+  return 0;
+}
